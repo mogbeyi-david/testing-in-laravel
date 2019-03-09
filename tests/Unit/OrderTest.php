c8cdd30334af91e5992b58bc3b39f48c650f222f
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-09 12:37:38 +0100

    Added testcase to determine if an order can determine the total cost of the products it contains

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -22,4 +22,17 @@ class OrderTest extends TestCase
 
         $this->assertCount(2, $order->products());
     }
+
+    public function test_that_an_order_can_determine_the_total_cost_of_all_its_products()
+    {
+        $order = new Order();
+
+        $firstProduct = new Product('Bible', 500);
+        $secondProduct = new Product('Laptop', '100');
+
+        $order->add($firstProduct);
+        $order->add($secondProduct);
+
+        $this->assertEquals(600, $order->total());
+    }
 }
