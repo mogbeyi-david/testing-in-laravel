5796c88999b20026aecfaf8254df6b2c46225877
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-09 17:55:13 +0100

    Limited the trending articles to five(5)

--- app/Article.php
+++ app/Article.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    public function scopeTrending($query)
+    public function scopeTrending($query, $take = 5)
     {
-        return $query->orderBy('reads', 'desc')->get();
+        return $query->orderBy('reads', 'desc')->take($take)->get();
     }
 }
